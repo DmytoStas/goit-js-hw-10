6ddec1f909d4acc115c04198d0ac64f4f1d54983
Dmytro Stas <dima7stas777@gmail.com>
2023-04-24 21:08:06 -0400

    project init

---
+++ src/js/makeListItemsMarkup.js
@@ -0,0 +1,6 @@
+// import countriesTpl from '../templates/listItemMarkup.hbs';
+// const countriesTpl = require('../templates/listItemMarkup.hbs');
+
+// export function makeListItemsMarkup({ countryList, countriesResp }) {
+//   countryList.insertAdjucentHTML('beforeend', countriesTpl(countriesResp));
+// }
